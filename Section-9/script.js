a46bd1e66c9ed4ced59c445b0a7371d8adb96684
kulkushw <kuldeep.kushwaha@publicissapient.com>
2023-02-28 20:15:48 +0530

    section-9

--- Section-9/script.js
+++ Section-9/script.js
@@ -212,3 +212,192 @@ restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
 restaurant.orderPizza('mushrooms');
 */
 
+
+///////////////////////////////////////
+// Maps: Iteration
+/*
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try again!'],
+]);
+console.log(question);
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+// Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+// const answer = Number(prompt('Your answer'));
+const answer = 3;
+console.log(answer);
+console.log(question.get(question.get('correct') === answer));
+// Convert map to array
+console.log([...question]);
+// console.log(question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]);
+*/
+///////////////////////////////////////
+// Maps: Fundamentals
+/*
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+const time = 8;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+console.log(rest.get(arr));*/
+///////////////////////////////////////
+// Sets
+/*
+const ordersSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Pizza',
+  'Risotto',
+  'Pasta',
+  'Pizza',
+]);
+console.log(ordersSet);
+console.log(new Set('Jonas'));
+console.log(ordersSet.size);
+console.log(ordersSet.has('Pizza'));
+console.log(ordersSet.has('Bread'));
+ordersSet.add('Garlic Bread');
+ordersSet.add('Garlic Bread');
+ordersSet.delete('Risotto');
+// ordersSet.clear();
+console.log(ordersSet);
+for (const order of ordersSet) console.log(order);
+// Example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+console.log(
+  new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
+);
+console.log(new Set('jonasschmedtmann').size);
+*/
+
+////////////////String-1///////////////////
+const airline='TaP Air India Air';
+const plane='B4ut';
+console.log(plane[2]);//u
+console.log(plane.length);//4
+console.log('Kuldeep'[2]);//l
+
+//length method
+console.log('Kuldeep'.length);//7 
+//index method
+console.log(airline.indexOf('P'));//2
+//last index
+console.log(airline.lastIndexOf('r'));//16
+//slice method
+console.log(airline.slice(4));//Air India Air
+console.group(airline.slice(4,7));//Air
+console.log(airline.slice(0,airline.indexOf(' ')));//TaP
+console.log(airline.slice(-2));//ir
+
+const middleSeat=function(seat){
+  const index=seat.slice(-1);
+  if(index=='B' || index == 'E'){
+    console.log('You got middle seat');
+  }
+  else{
+    console.log('You are lucky😎');
+  }
+}
+
+middleSeat('11B');
+middleSeat('11C');
+middleSeat('11E');
+///////////////////String-2///////////////
+const second='I am Here';
+//convert into lower case
+console.log(second.toLowerCase());//i am here
+// conert into upper case
+console.log(second.toUpperCase());// I AM HERE
+
+//Fix capitalization in name
+const name1='kulDeep';
+//TODO: TAsk is to change into 'Kuldeep';
+const toLower = name1.toLowerCase();//kuldeep
+const nameFix=toLower[0].toUpperCase()+toLower.slice(1);
+
+console.log(nameFix);//Kuldeep
+
+//Comparing emails
+const email='kushwa@gmail.com';
+const login='Kushwa@gmail.com \n';
+
+// to get rid off white space then use "TRIM " method;
+const normal=login.toLowerCase().trim();
+console.log(normal);//kushwa@gmail.com
+console.log(email===normal);//true
+
+//replacing
+const priceGB='288.89#';
+const priceUS=priceGB.replace('#','$').replace('.',',');
+console.log(priceUS);//288,89$
+
+const announce='All passengers come on 23 door, with ther luggage on 23 door number,';
+console.log(announce.replace('door','gate'));//All passengers come on 23 gate, with ther luggage on 23 door number,
+
+//herre we are using regex globally(g) to replace door to gate
+console.log(announce.replace(/door/g,'gate'));//All passengers come on 23 gate, with ther luggage on 23 gate number,
+
+//Boleans
+const plan ='A320neo';
+//includes
+console.log(plan.includes('A32'));//true
+console.log(plan.includes('Boeing'));//false
+//startsWith
+console.log(plan.startsWith('A32'))//true
+
+//split
+console.log('a+very+nice+guy+going'.split('+'));//['a', 'very', 'nice', 'guy', 'going']
+console.log('Kuldeep singh Kushwaha'.split(' '));
+
+const [firstName,,lastName] ='Kuldeep singh Kushwaha'.split(' ');
+
+console.log(firstName,lastName);
+//join method
+
+const newName=['Mr.',firstName,lastName].join(' ');
+console.log(newName);//Mr. Kuldeep Kushwaha
+
+//push method
+
+//padding method
+console.log('Ksk'.padStart(10,'='));//==========Ksk
+console.log('Ksk'.padEnd(10,'='));//Ksk==========
+
+//repeat method
+console.log('Bad weather'.repeat(4));
+
+//concat , reverse method
